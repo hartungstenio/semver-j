d2e22b148e3a6e04a05835546302d38522399692
Christian <6785871+hartungstenio@users.noreply.github.com>
2014-12-18 14:51:33 -0200

    Performance and documentation improvements (closes #5)
      - New version parser (Version.valueOf);
      - All classes and methods are documented;

--- src/net/hobbysw/semver/Version.java
+++ src/net/hobbysw/semver/Version.java
@@ -1,11 +1,11 @@
 package net.hobbysw.semver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
- * Implements the Semantic Versioning scheme.
+ * Implements the Semantic Versioning scheme. The format is MAJOR.MINOR.PATCH-pre.release+build
+ * 
  * 
  * @author Christian Hartung
  *
@@ -17,34 +17,106 @@ public class Version {
 	private List<String> preReleaseIdentifiers = new ArrayList<String>();
 	private List<String> buildMetadata = new ArrayList<String>();
 	
+	/**
+	 * Constructs a new <code>Version</code> with 0.0.0 as value
+	 */
+	public Version() {
+		this(0, 0, 0);
+	}
+	
+	/**
+	 * Constructs a new <code>Version</code> with the specified value
+	 * 
+	 * @param major the MAJOR version
+	 * @param minor the MINOR version
+	 * @param patch the PATCH version
+	 * 
+	 * @see	#setMajor
+	 * @see #setMinor
+	 * @see #setPatch
+	 */
+	public Version(int major, int minor, int patch) {
+		this.major = major;
+		this.minor = minor;
+		this.patch = patch;
+	}
+	
+	/**
+	 * Gets the MAJOR version
+	 * 
+	 * @return the MAJOR version number, which should be greater than or equal 0
+	 * @see	#setMajor
+	 */
 	public int getMajor() {
 		return major;
 	}
 	
+	/**
+	 * Sets the MAJOR version, which should be changed when you make incompatible API changes.
+	 *  
+	 * @param major the MAJOR version number, which must be an integer greater than or equal 0
+	 */
 	public void setMajor(int major) {
 		this.major = major;
 	}
 	
+	/**
+	 * Gets de MINOR version
+	 * 
+	 * @return the MINOR version number, which should be greater than or equal 0
+	 * @see	#setMinor
+	 */
 	public int getMinor() {
 		return minor;
 	}
 	
+	/**
+	 * Sets the MINOR version, which should be changed when you add functionality in a backwards-compatible manner.
+	 *  
+	 * @param minor the MINOR version number, which must be an integer greater than or equal 0
+	 */
 	public void setMinor(int minor) {
 		this.minor = minor;
 	}
 	
+	/**
+	 * Gets de PATCH version
+	 * 
+	 * @return the PATCH version number, which should be greater than or equal 0
+	 * @see	#setPatch
+	 */
 	public int getPatch() {
 		return patch;
 	}
 	
+	/**
+	 * Sets the PATCH version, which should be changed when you make backwards-compatible bug fixes.
+	 *  
+	 * @param minor the PATCH version number, which must be an integer greater than or equal 0
+	 */
 	public void setPatch(int patch) {
 		this.patch = patch;
 	}
 	
+	/**
+	 * Gets the pre-release identifiers list.
+	 * 
+	 * Pre-release identifiers are considered when comparing version numbers, and have lower precedence than
+	 * the associated normal versions.
+	 *  
+	 * @return the list of pre-release identifiers.
+	 */
 	public List<String> getPreReleaseIdentifiers() {
 		return preReleaseIdentifiers;
 	}
 	
+	/**
+	 * Gets the build metadata associated to this version.
+	 * 
+	 * Build metadata is ignored when determining version precedence.
+	 * 
+	 * @return the list of build metadata
+	 */
 	public List<String> getBuildMetadata() {
 		return buildMetadata;
 	}
@@ -57,11 +129,11 @@ public class Version {
 		
 		Version v = (Version)obj;
 
-		return     v.getMajor() == getMajor()
-				&& v.getMinor() == getMinor()
-				&& v.getPatch() == getPatch()
-				&& v.getPreReleaseIdentifiers().equals(getPreReleaseIdentifiers())
-				&& v.getBuildMetadata().equals(getBuildMetadata());
+		return     v.major == major
+				&& v.minor == minor
+				&& v.patch == patch
+				&& v.preReleaseIdentifiers.equals(preReleaseIdentifiers)
+				&& v.buildMetadata.equals(buildMetadata);
 				
 	}
 	
@@ -78,47 +150,76 @@ public class Version {
 		return result;
 	}
 	
-	public static Version valueOf(String str) throws VersionFormatException {
-		if(str == null) {
-			throw new VersionFormatException("null");
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(major).append('.').append(minor).append('.').append(patch);
+		
+		if(!preReleaseIdentifiers.isEmpty()) {
+			sb.append('-');
+			for(String s : preReleaseIdentifiers) {
+				sb.append(s).append('.');
+			}
+			sb.deleteCharAt(sb.length() - 1);
 		}
 		
-		String build = null;
-		String preRelease = null;
-		int major, minor, patch;
-		
-		String[] parts = str.split("+");
-		if(parts.length > 1) {
-			build = parts[1];
+		if(!buildMetadata.isEmpty()) {
+			sb.append('+');
+			
+			for(String s : buildMetadata) {
+				sb.append(s).append('.');
+			}
+			sb.deleteCharAt(sb.length() - 1);
 		}
 		
-		parts = parts[0].split("-");
-		if(parts.length > 1) {
-			preRelease = parts[1];
+		return sb.toString();
+	}
+	
+	/**
+	 * Converts a string in the format MAJOR.MINOR.PATCH-pre-release+build to a {@link Version}. Pre-release and build are both optional.
+	 * 
+	 * @param versionStr the version number as a string
+	 * @return a new version object with information extracted from the string
+	 * @throws VersionFormatException if the version does not conform to the MAJOR.MINOR.PATCH-pre-release+build format
+	 */
+	public static Version valueOf(String versionStr) throws VersionFormatException {
+		if(versionStr == null) throw new VersionFormatException("null");
+		if(versionStr.isEmpty()) throw new VersionFormatException("empty");
+		
+		final char[] versionChars = versionStr.toCharArray();
+		int begin = 0;
+		int current = 0;
+		int len = versionChars.length;
+		
+		boolean build = false;
+		boolean pr = false;
+		
+		Version result = new Version(-1, -1, -1);
+		
+		while(++current <= len) {
+			if(current == len || versionChars[current] == '.' || versionChars[current]== '-' || versionChars[current] == '+') {
+				String buf = String.valueOf(versionChars, begin, current - begin);
+				begin = current + 1;
+				
+				if(pr) {
+					result.preReleaseIdentifiers.add(buf);
+				} else if(build) {
+					result.buildMetadata.add(buf);
+				} else {
+					int val = Integer.parseInt(buf);
+					if(result.major < 0) result.major = val;
+					else if(result.minor < 0) result.minor = val;
+					else if(result.patch < 0) result.patch = val;
+					else throw VersionFormatException.forInputString(versionStr);
+				}
+				
+				if(current < len && versionChars[current] != '.') {
+					pr = (versionChars[current]== '-');
+					build = (versionChars[current] == '+');
+				}
+			}
 		}
 		
-		parts = parts[0].split("\\.");
-		if(parts.length != 3) {
-			throw VersionFormatException.forInputString(str);
-		}
-		
-		major = Integer.parseInt(parts[0]);
-		minor = Integer.parseInt(parts[1]);
-		patch = Integer.parseInt(parts[2]);
-		
-		Version ver = new Version();
-		ver.setMajor(major);
-		ver.setMinor(minor);
-		ver.setPatch(patch);
-		
-		if(preRelease != null) {
-			ver.getPreReleaseIdentifiers().addAll(Arrays.asList(preRelease.split(".")));
-		}
-		
-		if(build != null) {
-			ver.getBuildMetadata().addAll(Arrays.asList(build.split(".")));
-		}
-		
-		return ver;
+		return result;
 	}
 }
