59c56c7dd6cb14247fc35531b27e1b3ca10c35be
Christian Hartung <chrishartung@outlook.com>
2014-12-19 11:46:54 -0200

    Maven + UnitTest (closes #1, #6)
      - Project converted to a Maven Project (I guess);
      - Tried to create some Unit Tests

--- src/main/java/net/hobbysw/semver/VersionComparator.java
+++ src/main/java/net/hobbysw/semver/VersionComparator.java
@@ -12,38 +12,24 @@ import java.util.List;
  */
 public class VersionComparator implements Comparator<Version> {
 
-	@Override
 	public int compare(Version o1, Version o2) {
-		if(o1 == null) {
-			return o2 == null ? 0 : -1;
-		}
+		if(o1 == null) return o2 == null ? 0 : -1;
 		
 		int i = o1.getMajor() - o2.getMajor();
-		if(i != 0) {
-			return i;
-		}
+		if(i != 0) return i;
 		
 		i = o1.getMinor() - o2.getMinor();
-		if(i != 0) {
-			return i;
-		}
+		if(i != 0) return i;
 		
 		i = o1.getPatch() - o2.getPatch();
-		if(i != 0) {
-			return i;
-		}
+		if(i != 0) return i;
 		
 		return comparePreReleaseIdentifiers(o1.getPreReleaseIdentifiers(), o2.getPreReleaseIdentifiers());
 	}
 	
 	private int comparePreReleaseIdentifiers(List<String> v1, List<String> v2) {
-		if(v1.isEmpty()) {
-			return v2.isEmpty() ? 0 : 1;
-		}
-		
-		if(v2.isEmpty()) {
-			return v1.isEmpty() ? 0 : -1;
-		}
+		if(v1.isEmpty()) return v2.isEmpty() ? 0 : 1;
+		if(v2.isEmpty()) return v1.isEmpty() ? 0 : -1;
 		
 		int len1 = v1.size();
 		int len2 = v2.size();
@@ -52,12 +38,10 @@ public class VersionComparator implements Comparator<Version> {
 			try {
 				int n1 = Integer.parseInt(v1.get(i));
 				int n2 = Integer.parseInt(v2.get(i));
-				
-				if(n1 != n2) {
-					return n1 - n2;
-				}
+				if(n1 != n2) return n1 - n2;
 			} catch(NumberFormatException ex) {
-				
+				int j = v1.get(i).compareTo(v2.get(i));
+				if(j != 0) return j;
 			}
 		}
 		
